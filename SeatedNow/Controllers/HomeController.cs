a95711050fdacb8e1b4b6d6e6847eb016bb4479c
Rachael Hartman <rthartma@oakland.edu>
2018-01-22 21:30:28 -0500

    Enforce login page in HomeController.

--- SeatedNow/Controllers/HomeController.cs
+++ SeatedNow/Controllers/HomeController.cs
@@ -29,6 +29,13 @@ namespace SeatedNow.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            ViewData["Message"] = "Your login page.";
+
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
