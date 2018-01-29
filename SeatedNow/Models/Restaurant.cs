0ab7dc6fb9c553c84c3091fe97e353467a9df5de
mwnovak13 <mwnovak@oakland.edu>
2018-01-28 22:34:21 -0500

    Added to resturant.cs

--- SeatedNow/Models/Restaurant.cs
+++ SeatedNow/Models/Restaurant.cs
@@ -7,13 +7,31 @@ namespace SeatedNow.Models
 {
     public class Restaurant
     {
+        public RestaurantAccount(string name, string address, string city, string zip, string state , string phoneNumber)
+        {
+            Name = name;
+            Address = address;
+            City = city;
+            Zip = zip;
+            State = state;
+            PhoneNumber = phoneNumber;
+        }
+
         public int ID { get; set; }
 
         public string Name { get; set; }
 
         public string Address { get; set; }
 
+        public string City { get; set; }
 
-    
+        public string Zip { get; set; }
+
+        public string State { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+
+
     }
 }
