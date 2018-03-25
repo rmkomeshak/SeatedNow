77552dd59ebc0071a76bf22d3bc33cb5dd384059
Ryan Komeshak <rmkomeshak@oakland.edu>
2018-03-25 17:11:44 -0400

    small change

--- SeatedNow/Controllers/RestaurantController.cs
+++ SeatedNow/Controllers/RestaurantController.cs
@@ -177,7 +177,7 @@ namespace SeatedNow.Controllers
         public IActionResult UpdateAction(int Id, string Name, string Address, string City, string ZipCode, string State, string PhoneNumber, string ImagePath, string Description, string Color, int OwnerId, string EventKey, bool isVerified, List<String> Tags)
         {
 
-            _restaurantRepository.UpdateRestaurant(new Restaurant(Id, Name, Address, City, State, ZipCode, PhoneNumber, ImagePath, isVerified, OwnerId, EventKey, Description, Color, Tags));
+            _restaurantRepository.UpdateRestaurant(new Restaurant(Id, Name, Address, City, State, ZipCode, PhoneNumber, ImagePath, isVerified, OwnerId, EventKey, Description, Color));
             return Redirect("~/Restaurant/Dashboard");
         }
 
